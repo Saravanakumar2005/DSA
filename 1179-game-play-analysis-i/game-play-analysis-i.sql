a2184f938385573932ff40d00faca1378deb937f
Saravanakumar2005 <136157516+Saravanakumar2005@users.noreply.github.com>
2025-06-23 11:16:33 +0530

    Time: 459 ms (88.65%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,9 +1,6 @@
-with summa as(
-select player_id,event_date, ROW_NUMBER() OVER (Partition by player_id Order by event_date asc) AS first_login
-from Activity)
+select player_id, min(event_date)  AS first_login
+from Activity
+group by player_id
 
-select player_id,event_date as first_login
-from summa
-where first_login = 1
 
 
