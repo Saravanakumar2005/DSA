1405e2a3eb064f72739ce534bca4999d928f0963
Saravanakumar2005 <136157516+Saravanakumar2005@users.noreply.github.com>
2025-06-26 12:55:18 +0530

    Time: 3 ms (57.16%) | Memory: 43.4 MB (23.89%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.java
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -6,14 +6,14 @@ class Solution {
           
         }
 
-        if((temp.size()==4))  return ;
+        // if((temp.size()==4))  return ;
 
         for(int j=1;j<=3;j++){
             if(i+j >s.length()) break;
             String seg = s.substring(i,i+j); 
 
-            if(seg.charAt(0) =='0'&&seg.length()>1) continue;
-            if(Integer.parseInt(seg)>255) continue;
+            if(seg.charAt(0) =='0'&&seg.length()>1) break;
+            if(Integer.parseInt(seg)>255) break;
 
             temp.add(seg);
             solver(s,i+j,ans,temp);
@@ -26,6 +26,7 @@ class Solution {
         List<String> temp = new ArrayList<>();
         
         if(n>12) return ans;
+        if(n<4) return ans;
         solver(s,0,ans,temp);
         return ans;
     }
